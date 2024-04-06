0602045f9e2a1d9ed35171496b7f39a4f126f535
janmarsino98 <wiskyscapit@gmail.com>
2024-04-06 23:57:55 +0200

    Added requirements

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "La Seu",
+        "salary": "20000"
+    },
+    {
+        "id": 2,
+        "title": "Data Engineer",
+        "location": "Lleida",
+        "salary": "50000"
+    },
+    {
+        "id": 3,
+        "title": "Data Analyst",
+        "location": "Puigcerdà",
+        "salary": "30000"
+    },
+]
 
-def hello_world():
-    return "Hello world"
+@app.route("/")
+def hello_jan():
+    return render_template('jan.html', jobs = JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+    
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
